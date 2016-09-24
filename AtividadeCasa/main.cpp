65a2bddb6f94f54360b1b5dbcf0f27363b2463a3
Daniel Ramos <Daniel Ramos>
2016-09-23 21:37:44 -0300

    Corrigido bug ao printar linha

--- AtividadeCasa/main.cpp
+++ AtividadeCasa/main.cpp
@@ -5,14 +5,16 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "QDebug"
 #include <time.h>
+
 #define tipoQuadrado 0
 #define tipoCirculo  1
-#define MAX 1
+#define MAX 40
 #define NIVELMAXIMOCOR 255 // Branco
 #define NIVELMINIMOCOR 0 // Preto
+#define QTDOBJ 4
+
 using namespace cv;
 using namespace std;
-
 //--------------------------------------------------------X----------------------------------------------------------//
 
 
@@ -24,8 +26,10 @@ int main(int argc, char *argv[])
     srand(time(NULL));
     FILE * planilha;
     char nome[30];
+    int corte1;
 
     planilha=fopen("dados.csv","w");
+    fprintf(planilha,"Tipo;altura;largura;larg1;larg2;larg3\n");
 
     //gerando MAX imagens
     for(k=0;k<MAX;k++)
@@ -49,27 +53,40 @@ int main(int argc, char *argv[])
 
         //gerando um quadrado de tamanho aleatorio
         case tipoQuadrado:
+
             dim=rand()%300;
+            corte1=dim/4;
             ini=rand()%200;
+            //printf("dim %d \n",dim);
+            //printf("ini %d \n",ini);
             rectangle(imagem,Point(ini,ini),Point(ini+dim,ini+dim),Scalar(255),-1,8,0);
 
+            for(int i=0;i<QTDOBJ;i++){
+                line(imagem, Point(ini,ini+corte1*i), Point(dim+ini,ini+corte1*i),Scalar(0,255,0),1,8);
+            }
+
+            //printf("dim+ini %d \n",dim+ini);
             //imprimindo dados no arquivo .csv
             fprintf(planilha,"%d;",tipoQuadrado);
+
+            fprintf(planilha,"%d;",dim/4);
+            fprintf(planilha,"%d;",dim); // Ele considera 1 pixel na largura
             fprintf(planilha,"%d;",dim);
             fprintf(planilha,"%d;",dim);
             fprintf(planilha,"%d;",dim);
-            fprintf(planilha,"%d;",dim);
-            fprintf(planilha,"%d;",dim);
+
             fprintf(planilha,"\n");
             break;
 
-        //gerando um circulo de tamanho aleatorio
+            //gerando um circulo de tamanho aleatorio
         case tipoCirculo:
-            printf("Circulo Gerado\n");
+            //printf("Circulo Gerado\n");
             pos=(imagem.cols/2)+rand()%100;
             raio=rand()%90+10;
-            printf("Posicao Gerada: %d\n", pos);
+            //printf("Posicao Gerada: %d\n", pos);
             circle(imagem,Point(pos,pos),raio,Scalar(255),-1,8,0);
+
+            line(imagem, Point(pos/raio,pos), Point(raio+pos,pos),Scalar(0,255,0),1,8);
 
             //imprimindo dados no arquivo .csv
             fprintf(planilha,"%d;",tipoCirculo);
@@ -77,65 +94,67 @@ int main(int argc, char *argv[])
             fprintf(planilha,"%d;",raio*2);
 
 
-            int ponto = (raio*2)/3;
 
-            printf("Raio: %d\n", raio);
-            printf("Diametro: %d\n", raio*2);
-            printf("Ponto: %d\n", ponto);
+            // printf("Raio: %d\n", raio);
+            // printf("Diametro: %d\n", raio*2);
+            // printf("Ponto: %d\n", ponto);
 
             int pontoLinhaInferior = pos + (raio/2)*1; // Meio redundante. Eu sei, mas é que eu havia pensado que era para ser 3 pedaços, não 4
             int pontoLinhaSuperior = pos - (raio/2)*1;
 
             int largura1, largura2;
 
-            printf("PontoLinhaInferior: %d\n", pontoLinhaInferior);
-            printf("PontoLinhaSuperior: %d\n", pontoLinhaSuperior);
-            printf("==============\n");
+            // printf("PontoLinhaInferior: %d\n", pontoLinhaInferior);
+            // printf("PontoLinhaSuperior: %d\n", pontoLinhaSuperior);
+            // printf("==============\n");
 
-                for(int LINHA = 0; LINHA < imagem.rows; LINHA ++) {
-                    for(int COLUNA = 0; COLUNA < imagem.cols; COLUNA ++){
+            for(int LINHA = 0; LINHA < imagem.rows; LINHA ++) {
+                for(int COLUNA = 0; COLUNA < imagem.cols; COLUNA ++){
 
+                    if (LINHA == pontoLinhaInferior && COLUNA == pos) {
+                        // printf("Estamos na parte Inferior! Linha : %d  Coluna: %d\n", LINHA, COLUNA);
 
-                        if (LINHA == pontoLinhaSuperior && COLUNA == pos) {
+                        for (int NOVACOLUNA = 0; NOVACOLUNA < imagem.cols; NOVACOLUNA++) {
 
-                            for (int NOVACOLUNA = 0; NOVACOLUNA < imagem.cols; NOVACOLUNA++) {
+                            // printf("Nivel de cor em Linha %d Coluna %d eh %d\n", LINHA, NOVACOLUNA, imagem.at<uchar>(LINHA, NOVACOLUNA));
 
-                                imagem.at<uchar>(LINHA, NOVACOLUNA) = NIVELMINIMOCOR;
+                            if (imagem.at<uchar>(LINHA, NOVACOLUNA) == NIVELMAXIMOCOR &&
+                                    imagem.at<uchar>(LINHA, NOVACOLUNA+1) == NIVELMINIMOCOR  &&
+                                    imagem.at<uchar>(LINHA, NOVACOLUNA-1) == NIVELMAXIMOCOR) {
+                                largura2 = NOVACOLUNA;
+                                //   printf("Largura1 %d", largura1);
                             }
+
+                            if (imagem.at<uchar>(LINHA, NOVACOLUNA) == NIVELMAXIMOCOR &&
+                                    imagem.at<uchar>(LINHA, NOVACOLUNA- 1) == NIVELMINIMOCOR &&
+                                    imagem.at<uchar>(LINHA, NOVACOLUNA + 1) == NIVELMAXIMOCOR) {
+                                largura1 = NOVACOLUNA;
+                            }
+
+
                         }
 
 
+                        for (int NOVACOLUNA = 0; NOVACOLUNA < imagem.cols; NOVACOLUNA++) {
+                            imagem.at<uchar>(LINHA, NOVACOLUNA) = NIVELMINIMOCOR;
+                        }
+                    }
 
-                        if (LINHA == pontoLinhaInferior && COLUNA == pos) {
-                            printf("Estamos na parte Inferior! Linha : %d  Coluna: %d\n", LINHA, COLUNA);
+                    if (LINHA == pontoLinhaSuperior && COLUNA == pos) {
 
-                            for (int NOVACOLUNA = 0; NOVACOLUNA < imagem.cols; NOVACOLUNA++) {
-
-                                printf("Nivel de cor em Linha %d Coluna %d eh %d\n", LINHA, NOVACOLUNA, imagem.at<uchar>(LINHA, NOVACOLUNA));
-
-                                if (imagem.at<uchar>(LINHA, NOVACOLUNA) == NIVELMAXIMOCOR &&
-                                        imagem.at<uchar>(LINHA, NOVACOLUNA+1) == NIVELMAXIMOCOR  &&
-                                        imagem.at<uchar>(LINHA, NOVACOLUNA-1) == NIVELMINIMOCOR) {
-                                    largura1 = NOVACOLUNA;
-                                    printf("Largura1 %d", largura1);
-                                } else if (imagem.at<uchar>(LINHA, NOVACOLUNA) == NIVELMAXIMOCOR &&
-                                           imagem.at<uchar>(LINHA, NOVACOLUNA- 1) == NIVELMAXIMOCOR &&
-                                           imagem.at<uchar>(LINHA, NOVACOLUNA + 1) == NIVELMINIMOCOR) {
-                                    largura2 = NOVACOLUNA;
-                                }
-
-                                imagem.at<uchar>(LINHA, NOVACOLUNA) = NIVELMINIMOCOR;
-                            }
-                       }
-
-
+                        for (int NOVACOLUNA = 0; NOVACOLUNA < imagem.cols; NOVACOLUNA++) {
+                            imagem.at<uchar>(LINHA, NOVACOLUNA) = NIVELMINIMOCOR;
+                        }
                     }
                 }
+            }
 
-                fprintf(planilha,"%d;",largura2 - largura1);
-                fprintf(planilha,"%d;",raio*2);
-                fprintf(planilha,"%d;",largura2 - largura1);
-                fprintf(planilha,"\n");
+            // printf("Largura1 : %d", largura1);
+            // printf("Largura2 : %d", largura2);
+            fprintf(planilha,"%d;",largura2 - largura1);
+            fprintf(planilha,"%d;",raio*2);
+            fprintf(planilha,"%d;",largura2 - largura1);
+            fprintf(planilha,"\n");
 
             break;
         }
@@ -144,12 +163,12 @@ int main(int argc, char *argv[])
         //imagem0.jpg, imagem1.jpg ...
         sprintf(nome,"imagem%d.jpg",k);
 
-      //  namedWindow("teste",WINDOW_NORMAL);
-       // resizeWindow("teste",200,200);
+        //  namedWindow("teste",WINDOW_NORMAL);
+        // resizeWindow("teste",200,200);
 
         imwrite(nome,imagem);
 
-     //   imshow("teste",imagem);
+        //   imshow("teste",imagem);
         waitKey(0);
     }
 
